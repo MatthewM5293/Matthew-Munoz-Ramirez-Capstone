9cb22b262dd5e1bef61c7b727fdfd12b29ad48d9
MatthewM5293 <mmunozramirez@student.neumont.edu>
2023-10-09 22:43:59 -0600

    Profile can now be edited

--- Echoes_v0.1/Controllers/HomeController.cs
+++ Echoes_v0.1/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Echoes_v0._1.Models;
 using Echoes_v0._1.Interfaces;
 using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
 
 namespace Echoes_v0._1.Controllers;
 
@@ -40,7 +41,76 @@ public class HomeController : Controller
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
-    
-    
-    
+
+    [Authorize]
+    [HttpGet]
+    public IActionResult Profile()
+    {
+        UserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        ViewBag.UserId = UserId;
+
+        var id = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        ApplicationUser up = dal.GetUser(id);
+
+
+        return View("Profile/Profile", up);
+    }
+
+    public IActionResult ProfilePost(string? id)
+    {
+        UserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        ViewBag.UserId = UserId;
+
+        if (id == null) return NotFound();
+
+        ApplicationUser foundUser = dal.GetUser(id);
+        if (foundUser == null) return NotFound();
+
+        return View("Profile/ProfilePost" ,foundUser);
+    }
+
+    [Authorize]
+    [HttpGet]
+    public IActionResult EditProfile(string? id)
+    {
+        id = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        if (id == null)
+            return NotFound();
+
+        ApplicationUser foundUser = dal.GetUser(id);
+
+        if (foundUser == null) return NotFound();
+
+        return View("Profile/EditProfile", foundUser);
+    }
+
+    [HttpPost]
+    public IActionResult EditProfile(ApplicationUser ap)
+    {
+        //will have to save Pfp, Username, Fn, Ln, and About seperately
+
+        string temp = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+        ApplicationUser foundUser = dal.GetUser(temp);
+        if (foundUser == null) return NotFound();
+
+        if (!ap.ProfilePicture.Contains(".png") || !ap.ProfilePicture.Contains(".jpg")) ModelState.AddModelError("Invalid Image Format", "Invalid Format");
+
+        if (!ModelState.IsValid)
+        {
+            return View("Profile/EditProfile", foundUser);
+        }
+        else
+        {
+            foundUser.ProfilePicture = ap.ProfilePicture;
+            foundUser.UserName = ap.UserName;
+            foundUser.Name = ap.Name;
+            foundUser.DateOfBirth = ap.DateOfBirth;
+            foundUser.Bio = ap.Bio;
+            dal.EditUser(foundUser);
+        }
+
+        return RedirectToAction("Profile", "Home");
+    }
+
 }
--- Echoes_v0.1/Models/ApplicationUser.cs
+++ Echoes_v0.1/Models/ApplicationUser.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 using System.Drawing;
 
 namespace Echoes_v0._1.Models;
@@ -18,14 +19,32 @@ public class ApplicationUser : IdentityUser
 
     //Will be image later
 
+
+    [Required]
+    [DataType(DataType.ImageUrl)]
+    [Display(Name = "Profile Picture")]
     public string ProfilePicture { get; set; } = "https://cdn-icons-png.flaticon.com/512/3177/3177440.png";
     //public Image ProfilePicture { get; set; } = Image.FromFile("Resources\\3177440.png");
+
+    [Required]
+    [StringLength(30, ErrorMessage = "Username must be between 2-16 characters", MinimumLength = 1)]
+    [DataType(DataType.Text), RegularExpression(@"^[A-Za-z0-9_.]*$")]
+    [Display(Name = "Username")]
     public string Uname { get; set; } //Username for Application
-    
+
+
+    [Required]
+    [DataType(DataType.Text)]
+    [Display(Name = "Name")]
+    [RegularExpression(@"^[A-Za-z0-9@~`!@#$%^&*()_=+\\\\';:\""\\/?>.<, -]*$")]
     public string Name { get; set; }
-    
+
+
+    [Display(Name = "Bio")]
     public string Bio { get; set; } = "";
-    
+
+    [DataType(DataType.Date)]
+    [Display(Name = "Birthday")]
     public DateTime? DateOfBirth { get; set; }
     
     //Posts
